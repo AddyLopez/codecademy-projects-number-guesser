0dc78371bcabb8394f5d1dce0aa28bd7fc831983
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2022-07-19 19:21:37 -0600

    Powered the game with JS functions
    This game could be improved by preventing inputted numbers that are out of range (i.e. values that are not between 0 and 9 inclusive). However, that feature is out of scope for this project's learning objective, so I settled for an alert that calls attention to out of range values.

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -2,5 +2,36 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+const generateTarget = () => {
+  target = Math.floor(Math.random() * 10); // variable declared in game.js line 1
+};
 
+const alertOutOfRange = (currentHumanGuess) => {
+  if (currentHumanGuess > 9 || currentHumanGuess < 0) {
+    alert(
+      "That value is out of range. Please input a value between 0 and 9, inclusive."
+    );
+  }
+};
+
+const compareGuesses = (currentHumanGuess, computerGuess, target) => {
+  const humanGuessDistanceFromTarget = Math.abs(target - currentHumanGuess);
+  const computerGuessDistanceFromTarget = Math.abs(target - computerGuess);
+  if (humanGuessDistanceFromTarget <= computerGuessDistanceFromTarget) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const updateScore = (winner) => {
+  if ((winner = "human")) {
+    humanScore = humanScore + 1;
+  } else if ((winner = "computer")) {
+    computerScore = computerScore + 1;
+  }
+};
+
+const advanceRound = () => {
+  currentRoundNumber = currentRoundNumber + 1;
+};
